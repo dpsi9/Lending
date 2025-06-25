24e0928b2adc05d0a7e8951432b5d7bf5dcd2673
Dipendra Singh <dipendras953@gmail.com>
2025-06-25 17:40:07 +0530

    added initialize, deposit and withdraw

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,16 +1,32 @@
 use anchor_lang::prelude::*;
-
+use instructions::*;
+mod instructions;
+mod state;
+mod error;
 declare_id!("6BUoL9HNuSHtju5PyYvcpEZhs1KhGwF3nCxin3AG1NbF");
 
 #[program]
 pub mod lending {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn init_bank(
+        ctx: Context<InitBank>,
+        liquidation_threshold: u64,
+        max_ltv: u64,
+    ) -> Result<()> {
+        process_init_bank(ctx, liquidation_threshold, max_ltv)
+    }
+
+    pub fn init_user(ctx: Context<InitUser>, usdc_address: Pubkey) -> Result<()> {
+        process_init_user(ctx, usdc_address)
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        process_deposit(ctx, amount)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()>{
+        process_withdraw(ctx, amount)
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
+ 
