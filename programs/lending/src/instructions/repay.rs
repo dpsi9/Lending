e07a07a0fdc8dc90dc1b76b8faafa5ad7a6ac5e9
Dipendra Singh <dipendras953@gmail.com>
2025-06-26 16:20:26 +0530

    added repay functionality

---
+++ programs/lending/src/instructions/repay.rs
@@ -0,0 +1,101 @@
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{self, Mint, TokenAccount, TokenInterface, TransferChecked},
+};
+
+use crate::{error::ErrorCode, state::*};
+
+#[derive(Accounts)]
+pub struct Repay<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
+    #[account(
+        mut,
+        seeds =[mint.key().as_ref()],
+        bump
+    )]
+    pub bank: Account<'info, Bank>,
+    #[account(
+        mut,
+        seeds = [b"treasury", mint.key().as_ref()],
+        bump
+    )]
+    pub bank_token_account: InterfaceAccount<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds = [signer.key().as_ref()],
+        bump
+    )]
+    pub user_account: Account<'info, User>,
+    #[account(
+        init_if_needed,
+        payer = signer,
+        associated_token::mint = mint,
+        associated_token::authority = signer,
+        associated_token::token_program = token_program,
+    )]
+    pub user_token_account: InterfaceAccount<'info, TokenAccount>,
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+pub fn process_repay(ctx: Context<Repay>, amount: u64) -> Result<()> {
+    let user = &mut ctx.accounts.user_account;
+
+    let borrowed_assets: u64;
+
+    match ctx.accounts.mint.to_account_info().key() {
+        key if key == user.usdc_address => {
+            borrowed_assets = user.borrowed_usdc;
+        }
+        _ => {
+            borrowed_assets = user.borrowed_sol;
+        }
+    }
+
+    if amount > borrowed_assets {
+        return Err(ErrorCode::OverRepay.into());
+    }
+
+    let transfer_cpi_accounts = TransferChecked {
+        from: ctx.accounts.user_token_account.to_account_info(),
+        to: ctx.accounts.bank_token_account.to_account_info(),
+        mint: ctx.accounts.mint.to_account_info(),
+        authority: ctx.accounts.signer.to_account_info(),
+    };
+
+    let cpi_program = ctx.accounts.token_program.to_account_info();
+
+    let cpi_context = CpiContext::new(cpi_program, transfer_cpi_accounts);
+    let decimals = ctx.accounts.mint.decimals;
+
+    token_interface::transfer_checked(cpi_context, amount, decimals)?;
+
+    let bank = &mut ctx.accounts.bank;
+
+    let borrowed_ratio = amount.checked_div(bank.total_borrowed).unwrap();
+    let user_shares = bank
+        .total_borrowed_shares
+        .checked_mul(borrowed_ratio)
+        .unwrap();
+
+    let user = &mut ctx.accounts.user_account;
+
+    match ctx.accounts.mint.to_account_info().key() {
+        key if key == user.usdc_address => {
+            user.borrowed_usdc -= amount;
+            user.borrowed_usdc_shares -= user_shares;
+        }
+        _ => {
+            user.borrowed_sol -= amount;
+            user.borrowed_sol_shares -= user_shares;
+        }
+    }
+
+    bank.total_borrowed -= amount;
+    bank.total_borrowed_shares -= user_shares;
+    Ok(())
+}
